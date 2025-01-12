239b4613e3ad7cd2fa8df0b2c374d5bf4741c448
simoncampos1022 <simon.campos1022@gmail.com>
2025-01-11 17:35:37 -0800

    implement fill form

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,18 +1,32 @@
 import Header from '@/components/shared/Header'
 import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants'
+import { getUserById } from '@/lib/actions/user.actions';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const AddTransformationTypePage = ({ params: { type }}: SearchParamProps) => {
+const AddTransformationTypePage = async ({ params: { type }}: SearchParamProps) => {
   const transformation = transformationTypes[type];
+  const { userId } = await auth();
+
+  if(!userId) redirect('/sign-in')
+    
+  const user = getUserById(userId);
 
   return (
+
     <>
       <Header
         title={transformation.title}
         subtitle= {transformation.subTitle}
       />
-      <TransformationForm />
+      <TransformationForm
+        action='Add'
+        userId={user._id}
+        type={transformation.type as TransformationTypeKey}
+        creditBalance={user.creditBalance}
+      />
     </>
   )
 }
